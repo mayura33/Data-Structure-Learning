411f62d05222ec5c67f0fd201c95cceddb4893a6
Mayur Asodariya <mayurasodariya33@gmail.com>
2021-09-08 14:44:03 +0530

    Initial commit.!

--- Datastructure/Program.cs
+++ Datastructure/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Datastructure
 {
@@ -6,7 +10,6 @@ namespace Datastructure
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
