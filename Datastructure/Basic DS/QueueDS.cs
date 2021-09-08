685ab73b965f9d2e1a4277b799245530581ef49a
Mayur Asodariya <mayurasodariya33@gmail.com>
2021-09-08 15:45:36 +0530

    Add Stack and queue

---
+++ Datastructure/Basic DS/QueueDS.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datastructure.Basic_DS
+{
+    //FIFO
+    public class QueueDS
+    {
+        int maxSize;
+        int[] queArray;
+        int front;
+        int rear;
+        int currentCount;
+
+        public QueueDS(int size)
+        {
+            maxSize = size;
+            queArray = new int[size];
+            front = 0;
+            rear = -1;
+            currentCount = -1;
+        }
+        public static void Execute(int size)
+        {
+
+            QueueDS myQuue = new QueueDS(size);
+            Console.WriteLine("Executing From : " + myQuue.GetType().Name);
+            myQuue.Enqueue(100);
+            myQuue.Enqueue(200);
+            myQuue.Enqueue(300);
+            myQuue.Enqueue(400);
+            myQuue.Enqueue(500);
+
+            myQuue.Dequeue();
+            myQuue.Dequeue();
+            myQuue.Enqueue(600);
+
+            myQuue.View();
+        }
+        public void Enqueue(int num)
+        {
+            if (rear == maxSize - 1)
+            {
+                Console.WriteLine("Here Stack is Full so setting top=-1 || Num is : " + num);
+                rear = -1;
+            }
+            rear++;
+            queArray[rear] = num;
+            currentCount++;
+        }
+
+        public int PeakFront()
+        {
+            return queArray[front];
+        }
+        public bool IsEmpty()
+        {
+            return maxSize == currentCount;
+        }
+        public int Dequeue()
+        {
+            int temp = queArray[front];
+            queArray[front] = 0;
+            front++;
+            if (front == maxSize)
+            {
+                front = 0; //So we can utilize again an entire array
+            }
+            currentCount--;
+            return temp;
+        }
+
+        public void View()
+        {
+            Console.Write("[");
+            for (int i = 0; i < queArray.Length; i++)
+            {
+                Console.Write(queArray[i] + " ");
+            }
+            Console.Write("]");
+        }
+    }
+}
+
---
+++ Datastructure/Basic DS/StackDS.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datastructure.Basic_DS
+{
+    //LIFO
+    public class StackDS
+    {
+        int maxSize;
+        int[] stackArray;
+        int top;
+
+        public StackDS(int size)
+        {
+            maxSize = size;
+            stackArray = new int[size];
+            top = -1;
+        }
+
+        public static void Execute(int size)
+        {
+            StackDS myStack = new StackDS(size);
+            Console.WriteLine("Executing From : " + myStack.GetType().Name);
+            myStack.Pop();
+            myStack.Push(100);
+            myStack.Push(200);
+            myStack.Push(300);
+            myStack.Push(400);
+            myStack.Push(500);
+            myStack.Push(600);
+            myStack.Push(700);
+            myStack.View();
+        }
+
+        public void Push(int num)
+        {
+            if (IsFull())
+            {
+                Console.WriteLine("Here Stack is Full so setting top=-1 || Num is : " + num);
+                top = -1;
+            }
+
+            top++;
+            stackArray[top] = num;
+
+        }
+
+        public int Pop()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Stack is emptry, Operation aborted");
+                return -1;
+            }
+
+            int old_top = top;
+            top--;
+            return stackArray[old_top];
+        }
+
+        public int Peak()
+        {
+            return stackArray[top];
+        }
+
+        public bool IsEmpty()
+        {
+            return top == -1;
+        }
+
+        public bool IsFull()
+        {
+            return top == (maxSize - 1);
+        }
+
+        public void View()
+        {
+            Console.Write("[");
+            for (int i = 0; i < stackArray.Length; i++)
+            {
+                Console.Write(stackArray[i] + " ");
+            }
+            Console.Write("]");
+        }
+    }
+}
